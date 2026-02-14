687a89894fb21a641e313ed8bafff7af12923db5
MightyFine001 <jjuarez0087@gmail.com>
2026-02-14 03:19:17 -0800

    Gizmo Page Complete

--- Items.js
+++ Items.js
@@ -9,6 +9,55 @@ const brick = ("../BlocksImages/Brick.png");
 const coinoriginal = ("../ItemsImages/Coin.png")
 const psound = new Audio ("../Sounds/PSwitch.ogg");
 const psoundinitial = new Audio ("../Sounds/Pressed.wav");
+const buttons  = document.querySelectorAll(".OFbutton");
+const switches = document.querySelectorAll(".OFswitch");
+const blocks   = document.querySelectorAll(".OFblock");
+const buttonOn  = "../GizmosImages/OnBlock.gif";
+const buttonOff = "../GizmosImages/OffBlock.gif";
+const switchOn  = "../GizmosImages/ONOFFSwitchPressed.png";
+const switchOff = "../GizmosImages/ONOFFSwitch.gif";
+const blockOn   = "../GizmosImages/RedDottedBlockOn.png";
+const blockOff  = "../GizmosImages/RedDottedBlockOff.png";
+const toggleSound = new Audio("../Sounds/OnOffSwitch.wav");
+toggleSound.volume = 0.3;
+let isOn = true;
+function updateAll() {
+
+  const bImg = isOn ? buttonOn : buttonOff;
+  const sImg = isOn ? switchOn : switchOff;
+  const blImg = isOn ? blockOn : blockOff;
+
+  buttons.forEach(b => b.src = bImg);
+  switches.forEach(s => s.src = sImg);
+  blocks.forEach(bl => bl.src = blImg);
+}
+function toggleSystem() {
+  isOn = !isOn;
+
+  toggleSound.currentTime = 0;
+  toggleSound.play();
+
+  updateAll();
+}
+buttons.forEach(btn => {
+
+  btn.addEventListener("click", () => {
+
+    /* grow smoothly */
+    btn.style.transition = "transform 0.18s ease";
+    btn.style.transform = "scale(1.12)";
+
+    /* shrink back */
+    setTimeout(() => {
+      btn.style.transform = "scale(1)";
+    }, 180);
+
+    toggleSystem();
+  });
+
+});
+
+updateAll();
 psound.volume = 0.3;
 psoundinitial.volume = 0.1;
 let PClicked = false;
